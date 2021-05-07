6d8c8f9cdf54783c2d66dfef3083fabdafdfe760
rahmatramadh <rahmat.118140101@student.itera.ac.id>
2021-05-07 11:19:15 +0700

    edit detilpasien

--- detilpasien.jsx
+++ detilpasien.jsx
@@ -1,60 +1,178 @@
 import React,{useState} from "react";
 import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity, Image, StatusBar, ToastAndroid } from "react-native";
-import header from "assets/editpasien/header.jpg"
-import back from "assets/editpasien/back.jpg"
-import footer from "assets/editpasien/footer.jpg"
-import person from "assets/editpasien/person.jpg"
+import header from "assets/detadm/header.jpg";
+import back from "assets/detadm/back.jpg";
+import person from "assets/detadm/person.jpg";
+import box from "assets/detadm/box.jpg";
+import calendar from "assets/detadm/calendar.jpg";
+import lightblue from "assets/detadm/lightbluebox.jpg";
+import editbox from "assets/detadm/boxedit.jpg";
+import boxdet from "assets/detadm/boxdet.jpg";
 
-const Edit  = (props) => {
-  const [nama, setNama] = useState("");
-  const [umur, setUmur] = useState("");
-  const [alamat, setAlamat] = useState("");
-  const [jeniskelamin, setJenisKelamin] = useState("");
-  const [nohp, setNohp] = useState("");
-  
+const DetAdm  = (props) => {
     return (
+      <ScrollView style={{ backgroundColor: "white" }}>
         <View style={styles.container}>
             <Image style={styles.header}
             source = {{header}}>
             </Image>
             <Image style={styles.back}
             source = {{back}}>
-            <Text style={styles.header}>Rekam Medis</Text>
             </Image>
+            <Text style={styles.textheader}>Identitas Pasien</Text>
             <Image style={styles.person}
             source = {{person}}>
             </Image>
-            <Text style={styles.header}>Rekam Medis</Text>
-            <View style={styles.textInputWrapper}>
-                <TextInput style={styles.textInput} onChangeText={(nama) => setNama( nama )} value={nama} placeholder="Nama" placeholderTextColor="grey" />
-                <TextInput keyboardType="numeric" style={styles.textInput} onChangeText={(umur) => setUmur(umur)} value={umur} placeholder="Whatsapp" placeholderTextColor="grey" />
-                <DropDownPicker 
-                    placeholder="Jenis Kelamin"
-                    items={[
-                        {label: 'Laki-Laki', value: 'Laki-Laki'},
-                        {label: 'Perempuan', value: 'Perempuan'},
-                    ]}
-                    containerStyle={{height: 40}}
-                    style={{backgroundColor: "white"}}
-                    itemStyle={{
-                        justifyContent: 'flex-start'
-                    }}
-                    dropDownStyle={{backgroundColor: "white"}}
-                    onChangeItem={item => setJenisKelamin(...jeniskelamin,item.value)}
-                />
-                <TextInput style={styles.textInput} onChangeText={(alamat) => setAlamat( alamat )} value={alamat} placeholder="Nama" placeholderTextColor="grey" />
-                <TextInput style={styles.textInput} onChangeText={(nohp) => setNohp( nohp )} value={nohp} placeholder="Nama" placeholderTextColor="grey" />
-            </View>
-            <View style={styles.buttonWrapper}>
-                <TouchableOpacity style={styles.button} onPress={() => props.navigation.navigate("HomeCustomer")}>
-                <Text style={styles.buttonText}>Simpan Perubahan</Text>
-                </TouchableOpacity>
-            </View>
-            <Image style={styles.footer}
-            source = {{footer}}>
+            <Text style={styles.id}>Id Pasien</Text>
+            <Image style={styles.box}
+            source = {{box}}>
+            </Image>
+            <Text style={styles.rekam}>Rekam Medis</Text>
+            <Image style={styles.calendar}
+            source = {{calendar}}>
+            </Image>
+            <Text style={styles.rekam}>Tanggal</Text>
+            <Image style={styles.lightbluebox}
+            source = {{lightblue}}>
+                <Image style={styles.td}
+                source = {{boxdet}}>
+                </Image>
+                <Image style={styles.nadi}
+                source = {{boxdet}}>
+                </Image>
+                <Image style={styles.xy}
+                source = {{boxdet}}>
+                </Image>
+                <Image style={styles.suhu}
+                source = {{boxdet}}>
+                </Image>
+                <Image style={styles.bb}
+                source = {{boxdet}}>
+                </Image>
+                <Image style={styles.tb}
+                source = {{boxdet}}>
+                </Image>
+                <Image style={styles.xx}
+                source = {{boxdet}}>
+                </Image>
             </Image>
         </View>
+      </ScrollView>
     );
   }
 
+const styles = StyleSheet.create({
+    container: {
+      alignItems: "center",
+      paddingBottom: 50,
+    },
+    textheader:{
+        position: "absolute",
+        width: 168,
+        height: 28,
+        left: 233,
+        top: 80,
 
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: 300,
+        fontSize: 24,
+        lineHeight: 28,
+
+        color: "#000000",
+    },
+    header: {
+        position: "absolute",
+        width: 414,
+        height: 80,
+        left: 0,
+        top: 0,
+    },
+    id :{
+        position: "absolute",
+        width: 52,
+        height: 21,
+        left: 181,
+        top: 176,
+
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: 18,
+        lineHeight: 21,
+        color: "#000000",
+    },
+    rekam: {
+        position: "absolute",
+        width: 144,
+        height: 28,
+        left: 257,
+        top: 511,
+
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: 300,
+        fontSize: 24,
+        lineHeight: 28,
+
+        color: "#000000"
+    },
+    calendar: {
+        position: "absolute",
+        left: "3.86%",
+        right: "93.72%",
+        top: "61.94%",
+        bottom: "36.82%",
+
+        background: "#000000"
+    },
+    boxedit: {
+        position: "absolute",
+        width: 95,
+        height: 26,
+        left: 306,
+        top: 548,
+    },
+    lightbluebox: {
+        position: "absolute",
+        width: 414,
+        height: 315,
+        left: 0,
+        top: 581,
+    }, 
+    td: {
+        position: "absolute",
+        left: 33,
+        top: 624,
+    },
+    nadi: {
+        position: "absolute",
+        left: 153,
+        top: 624,
+    },
+    xy: {
+        position: "absolute",
+        left: 273,
+        top: 624,
+    },
+    suhu:{
+        position: "absolute",
+        left: 33,
+        top: 711,
+    },
+    bb: {
+        position: "absolute",
+        left: 153,
+        top: 711,
+    },
+    tb: {
+        position: "absolute",
+        left: 253,
+        top: 711,
+    },
+    xx: {
+        position: "absolute",
+        left: 153,
+        top: 798,
+    },
+  });
